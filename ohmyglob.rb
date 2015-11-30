7669d98fffff3bf9079b37ec80accae969c6ca43
Nicholas Colyer <ncolyer@gmail.com>
2015-11-30 01:55:49 -0500

    Update ohmyglob.rb
    Added compatibility for windows and arguments for source and destination directories.

--- ohmyglob.rb
+++ ohmyglob.rb
@@ -1,8 +1,63 @@
-# You can trash files by file extension. Simply add "trash" as the value for the file extension/hash key
+require 'optparse'
 
+# If windows, run windows equivalent commands for file move and delete operation
+if RUBY_PLATFORM =~ /mswin$|mingw32|mingw64|win32\-|\-win32/ then $is_nix = false else $is_nix = true end
+
+# Script Vars for Ref
 pwd = Dir.pwd
-base_dir = pwd # Or statically assign base dir ex: "/Users/ncolyer/Desktop"
 script_full_path = File.expand_path $0
+
+# Platform differences for actions
+def del_file(file)
+        if $is_nix == true
+                system 'rm', file
+        else
+                system 'del', file
+        end
+end
+def mov_file(src, dst)
+        if $is_nix == true
+                system 'mv', src, dst
+        else
+                system 'move', '/Y', src, dst
+        end
+end
+def mk_dir(dir)
+        if $is_nix == true
+                system 'mkdir', '-p', dir
+        else
+                system 'mkdir', dir
+        end
+end
+
+
+# Options parser
+options = {}
+OptionParser.new do |opts|
+        opts.banner = "Usage: ohmyglob.rb [options]"
+
+        opts.on("-s", "--srcdir SRC_DIR", "Set the source directory to be filed and categorized. (Typically your Downloads directory or cluttered directories.)") do |srcdirarg|
+                options[:src_dir] = srcdirarg
+        end
+        opts.on("-d", "--basedir BASE_DIR", "Set the relative base directory for the output of the script. (Typically your desktop directory.)") do |bdirarg|
+                options[:base_dir] = bdirarg
+        end
+        opts.on("-?", "--help", "Show this message") do
+                puts opts.help
+                exit
+        end
+end.parse!
+
+# IF basedir is not argv'd default to pwd
+base_dir = options[:base_dir]
+if base_dir == nil then
+        base_dir = pwd
+end
+# IF srcdir is not argv'd default to pwd
+src_dir = options[:src_dir]
+if src_dir == nil then
+        src_dir = pwd
+end
 
 ext = {
          "pdf" => '/documents/pdf/',
@@ -55,16 +110,16 @@ ext = {
 ext.each do |extension, file_dir|
 
         # Make Directory Structures
-        tmp_dir = base_dir + file_dir
-        system 'mkdir', '-p', tmp_dir
+        dst_dir = base_dir + file_dir
+        mk_dir(dst_dir)
 
-        Dir.glob("#{pwd}/*." + "#{extension}") do |cur_file|
+        Dir.glob("#{src_dir}/*." + "#{extension}") do |cur_file|
                 if cur_file != script_full_path
                         if file_dir != "trash"
-                            tmp_dir.downcase!
-                            system 'mv', cur_file, tmp_dir
+                            dst_dir.downcase!
+                            mov_file(cur_file, dst_dir)
                         else
-                            system 'rm', cur_file
+                            del_file(cur_file)
                         end
                 end
         end
