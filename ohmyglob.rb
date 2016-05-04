c0d87c348983705d0852ff4d5256908a644b47ae
ncolyer-r7 <ncolyer@rapid7.com>
2016-05-03 21:57:41 -0400

    Updated if/then's to shorthand and updated format

--- ohmyglob.rb
+++ ohmyglob.rb
@@ -9,54 +9,40 @@ script_full_path = File.expand_path $0
 
 # Platform differences for actions
 def del_file(file)
-        if $is_nix == true
-                system 'rm', file
-        else
-                system 'del', file
-        end
+  $is_nix == true ? (system 'rm', file) : (system 'del', file)
 end
 def mov_file(src, dst)
-        if $is_nix == true
-                system 'mv', src, dst
-        else
-                system 'move', '/Y', src, dst
-        end
+  $is_nix == true ? (system 'mv', src, dst) : (system 'move', '/Y', src, dst)
 end
 def mk_dir(dir)
-        if $is_nix == true
-                system 'mkdir', '-p', dir
-        else
-                system 'mkdir', dir
-        end
+  $is_nix == true ? (system 'mkdir', '-p', dir) : (system 'mkdir', dir)
 end
-
 
 # Options parser
 options = {}
 OptionParser.new do |opts|
-        opts.banner = "Usage: ohmyglob.rb [options]"
-
-        opts.on("-s", "--srcdir SRC_DIR", "Set the source directory to be filed and categorized. (Typically your Downloads directory or cluttered directories.)") do |srcdirarg|
-                options[:src_dir] = srcdirarg
-        end
-        opts.on("-d", "--basedir BASE_DIR", "Set the relative base directory for the output of the script. (Typically your desktop directory.)") do |bdirarg|
-                options[:base_dir] = bdirarg
-        end
-        opts.on("-?", "--help", "Show this message") do
-                puts opts.help
-                exit
-        end
+  opts.banner = "Usage: ohmyglob.rb [options]"
+  opts.on("-s", "--srcdir SRC_DIR", "Set the source directory to be filed and categorized. (Typically your Downloads directory or cluttered directories.)") do |srcdirarg|
+    options[:src_dir] = srcdirarg
+  end
+  opts.on("-d", "--basedir BASE_DIR", "Set the relative base directory for the output of the script. (Typically your desktop directory.)") do |bdirarg|
+    options[:base_dir] = bdirarg
+  end
+  opts.on("-?", "--help", "Show this message") do
+    puts opts.help
+    exit
+  end
 end.parse!
 
 # IF basedir is not argv'd default to pwd
 base_dir = options[:base_dir]
 if base_dir == nil then
-        base_dir = pwd
+  base_dir = pwd
 end
 # IF srcdir is not argv'd default to pwd
 src_dir = options[:src_dir]
 if src_dir == nil then
-        src_dir = pwd
+  src_dir = pwd
 end
 
 ext = {
@@ -109,20 +95,21 @@ ext = {
 
 ext.each do |extension, file_dir|
 
-        # Make Directory Structures
-        dst_dir = base_dir + file_dir
-        if file_dir != "trash" then
-                mk_dir(dst_dir)
-        end
+  # Make Directory Structures
+  dst_dir = base_dir + file_dir
+  if file_dir != "trash" then
+    mk_dir(dst_dir)
+  end
 
-        Dir.glob("#{src_dir}/*." + "#{extension}") do |cur_file|
-                if cur_file != script_full_path
-                        if file_dir != "trash"
-                            dst_dir.downcase!
-                            mov_file(cur_file, dst_dir)
-                        else
-                            del_file(cur_file)
-                        end
-                end
-        end
+  Dir.glob("#{src_dir}/*." + "#{extension}") do |cur_file|
+    if cur_file != script_full_path
+      if file_dir != "trash"
+        dst_dir.downcase!
+        mov_file(cur_file, dst_dir)
+      else
+        del_file(cur_file)
+      end
+    end
+  end
+  
 end
